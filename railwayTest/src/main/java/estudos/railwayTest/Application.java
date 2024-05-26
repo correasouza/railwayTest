ca9a3a788a387a1d6582f976b0d76258646cd57c
Igor Correa <igorsantosigor272@gmail.com>
2024-05-26 19:13:28 -0300

    add hibernate

--- railwayTest/src/main/java/estudos/railwayTest/Application.java
+++ railwayTest/src/main/java/estudos/railwayTest/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RailwayTestApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RailwayTestApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
