7e07cf6069424e7724ae99732d9c9ec500e98a12
Igor Correa <igorsantosigor272@gmail.com>
2024-05-26 19:22:34 -0300

    correção

--- railwayTest/src/main/java/estudos/railwayTest/domain/model/Account.java
+++ railwayTest/src/main/java/estudos/railwayTest/domain/model/Account.java
@@ -16,10 +16,10 @@ public class Account {
 
     private String agency;
 
-    @Column(name = "additional_limit", scale = 13, precision = 2)
+    @Column(name = "additional_limit", precision = 13, scale = 2)
     private BigDecimal limit;
 
-    @Column(scale = 13, precision = 2)
+    @Column(precision = 13, scale = 2)
     private BigDecimal balance;
 
     public Long getId() {
