b6d477496688d5cf4393f08a117c5b454db24a11
Igor Correa <igorsantosigor272@gmail.com>
2024-05-26 16:20:17 -0300

    Initial commit

---
+++ railwayTest/src/main/java/estudos/railwayTest/RailwayTestApplication.java
@@ -0,0 +1,13 @@
+package estudos.railwayTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayTestApplication.class, args);
+	}
+
+}
